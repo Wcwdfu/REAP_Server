adbd863e8ebc3780febd3b712777a0af80da5ad1
강병현 <kim1228com@naver.com>
2024-09-29 13:26:55 +0900

    [refactor] stt controller 인자 변경

--- src/main/java/Team_REAP/appserver/BH_file/STTController.java
+++ src/main/java/Team_REAP/appserver/BH_file/STTController.java
@@ -3,6 +3,7 @@ package Team_REAP.appserver.BH_file;
 import Team_REAP.appserver.BH_file.Service.STTService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import java.io.IOException;
 
 @RequiredArgsConstructor
 @RestController
+@Slf4j
 public class STTController {
     @Value("${naver.cloud.invoke.url}")
     private String apiUrl;
@@ -27,16 +29,10 @@ public class STTController {
     @Operation(summary = "음성 -> S3저장, 스크립트 변환 후 DB 저장")
     @PostMapping("/recognize-url")
     public ResponseEntity<String> recognizeMediaFromURL(@RequestParam("media") MultipartFile media,
-                                                        @RequestParam("user") String userName,
-                                                        @RequestParam("language") String language,
-                                                        @RequestParam(value = "completion", required = false, defaultValue = "sync") String completion, // default가 async이다.
-                                                        @RequestParam(value = "callback", required = false) String callback,
-                                                        @RequestParam(value = "wordAlignment", required = false, defaultValue = "true") boolean wordAlignment,
-                                                        @RequestParam(value = "fullText", required = false, defaultValue = "true") boolean fullText,
-                                                        @RequestParam(value = "resultToObs", required = false, defaultValue = "false") boolean resultToObs,
-                                                        @RequestParam(value = "noiseFiltering", required = false, defaultValue = "true") boolean noiseFiltering) throws IOException {
+                                                        @RequestParam("user") String userName) throws IOException {
 
+        ResponseEntity<String> response = sttService.audioToText(media, userName);
 
-        return sttService.audioToText(media, userName, language, completion, callback, wordAlignment, fullText, resultToObs, noiseFiltering);
+        return response;
     }
 }
--- src/main/java/Team_REAP/appserver/common/login/controller/UserController.java
+++ src/main/java/Team_REAP/appserver/common/login/controller/UserController.java
@@ -1,6 +1,11 @@
 package Team_REAP.appserver.common.login.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,12 +15,17 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/common/login")
+@Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
+    private final OAuth2AuthorizedClientService authorizedClientService;
     @GetMapping("/info")
     public String getJson(Authentication authentication) {
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
 
         Map<String, Object> attributes = oAuth2User.getAttributes();
+
 
         return attributes.toString();
     }
