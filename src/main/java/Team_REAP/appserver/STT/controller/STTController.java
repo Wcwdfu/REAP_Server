4fc5880122610e108c298c2b4e38043f6a13cd1c
강병현 <kim1228com@naver.com>
2024-10-29 05:54:09 +0900

    [fix] STT 클래스 리펙토링

--- src/main/java/Team_REAP/appserver/STT/controller/STTController.java
+++ src/main/java/Team_REAP/appserver/STT/controller/STTController.java
@@ -1,43 +1,42 @@
 package Team_REAP.appserver.STT.controller;
 
-import Team_REAP.appserver.DB.mongo.service.ScriptService;
-import Team_REAP.appserver.STT.service.STTService;
+import Team_REAP.appserver.STT.dto.AudioUploadDTO;
+import Team_REAP.appserver.STT.service.AudioService;
 import Team_REAP.appserver.common.login.ano.AuthUser;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
-@RequiredArgsConstructor
-@RestController
-@RequestMapping("/api")
 @Slf4j
+@RestController
+@RequiredArgsConstructor
 public class STTController {
 
-    @Value("${naver.cloud.invoke.url}")
-    private String apiUrl;
-
-    @Value("${naver.cloud.secret.key}")
-    private String secretKey;
-
-    private final STTService sttService;
-
-    private final ScriptService mongoUserService;
+    private final AudioService audioService;
 
 
     @Operation(summary = "음성 -> S3저장, 스크립트 변환 후 DB 저장", description = "사용자로부터 음성을 받으면 네이버 STT를 통해서 음성을 텍스트로 변환합니다." +
             "STT 결과를 가지고 스크립트로 만들어 음성 파일은 S3에 저장하고 스크립트 관련 내용은 MongoDb에 저장합니다.")
-    @PostMapping("/auth/uploadMediaFile")
+    @PostMapping("/auth/recognize-url")
     public ResponseEntity<Object> recognizeMediaFromURL(@RequestParam("media") MultipartFile media,
-                                                        @AuthUser String userName,
+                                                        @AuthUser String userId,
                                                         @RequestParam("topic") String topic) throws IOException {
 
-        ResponseEntity<Object> response = sttService.audioToText(media, userName, topic);
-        return response;
+
+        try {
+            AudioUploadDTO audioUploadDTO = audioService.processAudio(media, userId, topic);
+            return ResponseEntity.status(HttpStatus.OK).body(audioUploadDTO);
+        } catch (Exception e) {
+            log.error("Error processing audio", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error occurred: " + e.getMessage());
+        }
     }
 }
